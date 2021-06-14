fef3452b4c74215ce4093eb3e351eb008e9e50d0
Esteban Salomon <esalomon6492@southwestuniversity.edu>
2021-06-14 01:13:22 -0600

    CS Assignment 8

---
+++ CSAssignment8/CSAssignment8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSAssignment8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<int> myOwnClass = new MyClass<int>();
+            myOwnClass.AnyNumber = 11;
+
+            myOwnClass.PrintData(11);
+        }
+    }
+}
+
+public class MyClass<T>
+{
+    public T AnyNumber { get; set; }
+
+    public void PrintData(T parameter)
+    {
+        Console.WriteLine("Value: " + parameter);
+    }
+}
